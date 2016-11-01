c09e4ec4dae2f226ec2d6fe3b7c8f5f3c4ec9c21
seungwoogoh <vkqkf55@naver.com>
2016-11-01 13:28:36 +0900

    Create   TicTacToe_src_sample_BotGamer.java

---
+++ JAVA/TicTacToeGame/  TicTacToe_src_sample_BotGamer.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class BotGamer {
+    private String level;
+
+    public BotGamer(String level) {
+        this.level = level;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+}
